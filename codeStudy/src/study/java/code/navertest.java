e7897972551c8327886923814ba21cbacabd49bb
jinwon <sejew93@gmail.com>
2019-09-25 23:25:36 +0900

    [서진원]네이버 코딩테스트 업로드

---
+++ codeStudy/src/study/java/code/navertest.java
@@ -0,0 +1,11 @@
+package study.java.code;
+
+public class navertest {
+	public static void main(String[] args) {
+		int n = 3;
+		long answer = 0;
+		int [] test = {2,6,12,20,24,30,42,56,60,72};
+		answer = test[n];
+		System.out.println(answer);
+	}
+}
